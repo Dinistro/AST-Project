58507f5a43d37c58363a7eba5facf48cb896c7eb
Michel Weber <michel.weber@inf.ethz.ch>
2021-04-28 16:12:06 +0200

    make things generic

--- comp/if_folder/if_backup.cpp
+++
@@ -1,8 +0,0 @@
-void fuu() {
-  int i = 0;
-  if (!(i < 0)) {
-    i = 1;
-  } else {
-    i = -1;
-  }
-}
--- comp/if_folder/simple.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdio.h>
-
-int main() {
-  int sum = 0;
-  for (int i = 0; i < 10; ++i) {
-    sum += i;
-  }
-}
