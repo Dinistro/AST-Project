38ba01f9a25853778270f752b831999e5dd76005
Michel Weber <michel.weber@inf.ethz.ch>
2021-05-13 17:36:32 +0200

    chnage python script to apply powerset

--- compare.py
+++ compare.py
@@ -2,6 +2,9 @@
 import argparse
 import bz2
 
+import os
+import json
+
 import numpy as np
 import subprocess
 
@@ -10,6 +13,7 @@ parser.add_argument('--transformer', dest='transformer', required=False, default
         help='which transformer to run', nargs='+')
 parser.add_argument('--flag', dest='flag', required=False, default='', help='compiler flags to use', nargs='+')
 parser.add_argument('--printCmd', dest='printCmd', required=False, type=bool, const=True, default=False, help='print commands', nargs='?')
+parser.add_argument('--combo', dest='combo', required=False, type=bool, const=True, default=False, help='combine rewriters', nargs='?')
 parser.add_argument('--clangFlag', dest='clangFlag', required=False, default='', help='additional clang flags')
 parser.add_argument('--nodelete', dest='nodelete', required=False, type=bool, const=False, default=True, help='dont delete asm files', nargs='?')
 parser.add_argument('--compiler', dest='compiler', 
@@ -21,6 +25,8 @@ transformers = ['if', 'add', 'forToWhile']
 directory = './compf'
 
 toRun = []
+
+deleteRun = []
 
 f = open('build/bin/builtInInclude.path', 'r')
 if(not f):
@@ -34,52 +40,85 @@ if(args.transformer != 'all'):
     transformers = args.transformer
 
 subprocess.run('mkdir compf', cwd='./', text=True, timeout=30, shell=True)
-toRun.append('mkdir init_asm')
-runString = args.compiler + ' '
-for fl in args.flag:
-    runString += '-' + fl + ' '
-runString += '../examples/* -S'
-toRun.append(runString)
-toRun.append('mv *.s init_asm')
+
+def powerset(A):
+    if A == []:
+        return [[]]
+    a = A[0]
+    incomplete_pset = powerset(A[1:])
+    rest = []
+    for set in incomplete_pset:
+        rest.append([a] + set)
+    return rest + incomplete_pset
+
+
+if args.combo:
+    transformers_pset = powerset(transformers)
+else:
+    transformers_pset = [[x] for x in transformers]   
+    transformers_pset.append([])   
+
+for powset in transformers_pset:
+    powerstring = ''.join(powset)  
+    print(powerstring)
+    toRun.append('mkdir ' +  powerstring + '_folder')
+    toRun.append('mkdir ' +  powerstring + '_asm')
+    toRun.append('cp ../examples/* ' + powerstring + '_folder/')
+   
+    for trans in powset: 
+        #transforming
+        toRun.append('../build/bin/ast-project ' + powerstring + '_folder/* --' 
+                    + trans + ' -- -I' + includePath + ' ' + args.clangFlag)
     
-for trans in transformers:
-    # copying
-    toRun.append('mkdir ' +  trans + '_folder')
-    toRun.append('mkdir ' +  trans + '_asm')
-    toRun.append('cp ../examples/* ' + trans + '_folder/')
-
-    #transforming
-    toRun.append('../build/bin/ast-project ' + trans + '_folder/* --' 
-                + trans + ' -- -I' + includePath + ' ' + args.clangFlag)
-
     #compiling
+    #TODO: Do not compile files that are equal to initial
     runString = args.compiler + ' '
     for fl in args.flag:
         runString += '-' + fl + ' '
-    runString += trans + '_folder/* -S'
+    runString += powerstring + '_folder/* -S'
     toRun.append(runString)
-    toRun.append('mv *.s ' + trans + '_asm')
-
-    #comparing
-    toRun.append('diff -qrs init_asm/ ' + trans + '_asm/')
-
-    if args.nodelete:
+    toRun.append('mv *.s ' + powerstring + '_asm')
     
-        #cleaning
-        toRun.append('rm ' + trans + '_folder/*')
-        toRun.append('rm ' + trans + '_asm/*')
-        toRun.append('rmdir '+ trans + '_folder')
-        toRun.append('rmdir '+ trans + '_asm')
-
-if args.nodelete:
-    toRun.append('rm init_asm/*')
-    toRun.append('rmdir init_asm')
-
-
+    #cleaning
+    deleteRun.append('rm ' + powerstring + '_folder/*')
+    deleteRun.append('rm ' + powerstring + '_asm/*')
+    deleteRun.append('rmdir '+ powerstring + '_folder')
+    deleteRun.append('rmdir '+ powerstring + '_asm')
+    
 for cmd in toRun:
     if(args.printCmd):
         print(cmd)
     subprocess.run(cmd, cwd=directory, text=True, timeout=30, shell=True)
 
+binarySizes = {}
+
+if True:
+    currentSizes = []
+    powerstring = ''
+    print(powerstring)
+    for i in range(0,30):
+        currentSizes.append(os.path.getsize('compf/' + powerstring + '_asm/prog' + str(i) + '.s'))
+    binarySizes[powerstring] = currentSizes
+
+
+
+
+for powset in transformers_pset:
+    currentSizes = []
+    powerstring = ''.join(powset)
+    print(powerstring)
+    for i in range(0,30):
+        currentSizes.append(os.path.getsize('compf/' + powerstring + '_asm/prog' + str(i) + '.s'))
+    binarySizes[powerstring] = currentSizes
+
+print(binarySizes)
+f = open('result.txt', 'w') 
+f.write(json.dumps(binarySizes))
+
 if args.nodelete:
+    for cmd in deleteRun:
+        if(args.printCmd):
+            print(cmd)
+        subprocess.run(cmd, cwd=directory, text=True, timeout=30, shell=True)
+
     subprocess.run('rmdir compf', cwd='./', text=True, timeout=30, shell=True)
